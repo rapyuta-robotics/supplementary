c1e855750c76086ff7540fa43fb0cd1db8340f07
Karasuma1412 <bjoerninorek@gmail.com>
2020-10-12 12:03:06 +0200

    remove template functions

--- system_config/include/essentials/Configuration.h
+++ system_config/include/essentials/Configuration.h
@@ -11,11 +11,12 @@
 #include <string>
 #include <vector>
 
+#include "IAlicaConfig.h"
 #include "ConfigNode.h"
 
 namespace essentials
 {
-class Configuration
+class Configuration : public IAlicaConfig
 {
 protected:
     static const char LIST_ELEMENT_SEPERATOR = ',';
@@ -76,181 +77,181 @@ protected:
     std::shared_ptr<std::vector<std::string>> getNames(const char* path, ...);
     std::shared_ptr<std::vector<std::string>> tryGetNames(std::string d, const char* path, ...);
 
-    template <typename T>
-    T get(const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        if (nodes.size() == 0) {
-            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
-            std::cerr << errMsg << std::endl;
-            throw std::runtime_error(errMsg);
-        } else {
-            return convert<T>(nodes[0]->getValue());
-        }
-    }
-
-    template <typename T>
-    std::vector<T> getList(const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        if (nodes.size() == 0) {
-            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
-            std::cerr << errMsg << std::endl;
-            throw std::runtime_error(errMsg);
-        } else {
-            return convertList<T>(nodes[0]->getValue());
-        }
-    }
-
-    template <typename T>
-    std::shared_ptr<std::vector<T>> getAll(const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        if (nodes.size() == 0) {
-            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
-            std::cerr << errMsg << std::endl;
-            throw std::runtime_error(errMsg);
-        } else {
-            std::shared_ptr<std::vector<T>> result(new std::vector<T>());
-
-            for (int i = 0; i < nodes.size(); i++) {
-                result->push_back(convert<T>(nodes[i]->getValue()));
-            }
-
-            return result;
-        }
-    }
-
-    template <typename T>
-    T tryGet(T d, const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        if (nodes.size() == 0) {
-            return d;
-        }
-
-        return convert<T>(nodes[0]->getValue());
-    }
-
-    template <typename T>
-    std::shared_ptr<std::vector<T>> tryGetAll(T d, const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        std::shared_ptr<std::vector<T>> result(new std::vector<T>());
-
-        if (nodes.size() == 0) {
-
-            result->push_back(d);
-
-            return result;
-        }
-
-        for (int i = 0; i < nodes.size(); i++) {
-            result->push_back(convert<T>(nodes[i]->getValue()));
-        }
-
-        return result;
-    }
-
-    template <typename T>
-    void set(T value, const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
-        va_end(ap);
-
-        std::vector<ConfigNode*> nodes;
-
-        collect(this->configRoot.get(), params.get(), 0, &nodes);
-
-        for (int i = 0; i < nodes.size(); i++) {
-            if (nodes[i]->getType() == ConfigNode::Leaf) {
-                nodes[i]->setValue(stringify(value));
-            }
-        }
-    }
-
-
-    /**
-     * This method creates the configuration parameter if it not already exists
-     */
-    template <typename T>
-    void setCreateIfNotExistent(T value, const char* path, ...)
-    {
-        va_list ap;
-        va_start(ap, path);
-        std::shared_ptr<std::vector<std::string>> params_ptr = getParams('.', path, ap);
-        va_end(ap);
-
-        if (params_ptr->size() < 2) {
-            std::string errMsg = "SC-Conf: path cannot be created, not enough params";
-            std::cerr << errMsg << std::endl;
-            throw std::runtime_error(errMsg);
-        }
-
-        std::vector<ConfigNode*> nodes;
-        std::vector<std::string>& params = *params_ptr;
-        collect(this->configRoot.get(), &params, 0, &nodes);
-
-        if (!nodes.empty()) {
-            return;
-        }
-
-        ConfigNode* currentNode = this->configRoot.get();
-        size_t ind = 0;
-        while (ind < params.size()) {
-            auto children = currentNode->findChildren(params[ind]);
-            if (children.empty()) {
-                if (ind == params.size() - 1) {
-                    currentNode = currentNode->create(params[ind], stringify(value));
-                } else {
-                    currentNode = currentNode->create(params[ind]);
-                }
-            } else {
-                // We chose the first,  should be the only one !
-                currentNode = children[0].get();
-            }
-            ++ind;
-        }
-    }
+//    template <typename T>
+//    T get(const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        if (nodes.size() == 0) {
+//            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
+//            std::cerr << errMsg << std::endl;
+//            throw std::runtime_error(errMsg);
+//        } else {
+//            return convert<T>(nodes[0]->getValue());
+//        }
+//    }
+//
+//    template <typename T>
+//    std::vector<T> getList(const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        if (nodes.size() == 0) {
+//            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
+//            std::cerr << errMsg << std::endl;
+//            throw std::runtime_error(errMsg);
+//        } else {
+//            return convertList<T>(nodes[0]->getValue());
+//        }
+//    }
+//
+//    template <typename T>
+//    std::shared_ptr<std::vector<T>> getAll(const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        if (nodes.size() == 0) {
+//            std::string errMsg = "SC-Conf: " + pathNotFound(params.get());
+//            std::cerr << errMsg << std::endl;
+//            throw std::runtime_error(errMsg);
+//        } else {
+//            std::shared_ptr<std::vector<T>> result(new std::vector<T>());
+//
+//            for (int i = 0; i < nodes.size(); i++) {
+//                result->push_back(convert<T>(nodes[i]->getValue()));
+//            }
+//
+//            return result;
+//        }
+//    }
+//
+//    template <typename T>
+//    T tryGet(T d, const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        if (nodes.size() == 0) {
+//            return d;
+//        }
+//
+//        return convert<T>(nodes[0]->getValue());
+//    }
+//
+//    template <typename T>
+//    std::shared_ptr<std::vector<T>> tryGetAll(T d, const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        std::shared_ptr<std::vector<T>> result(new std::vector<T>());
+//
+//        if (nodes.size() == 0) {
+//
+//            result->push_back(d);
+//
+//            return result;
+//        }
+//
+//        for (int i = 0; i < nodes.size(); i++) {
+//            result->push_back(convert<T>(nodes[i]->getValue()));
+//        }
+//
+//        return result;
+//    }
+//
+//    template <typename T>
+//    void set(T value, const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params = getParams('.', path, ap);
+//        va_end(ap);
+//
+//        std::vector<ConfigNode*> nodes;
+//
+//        collect(this->configRoot.get(), params.get(), 0, &nodes);
+//
+//        for (int i = 0; i < nodes.size(); i++) {
+//            if (nodes[i]->getType() == ConfigNode::Leaf) {
+//                nodes[i]->setValue(stringify(value));
+//            }
+//        }
+//    }
+//
+//
+//    /**
+//     * This method creates the configuration parameter if it not already exists
+//     */
+//    template <typename T>
+//    void setCreateIfNotExistent(T value, const char* path, ...)
+//    {
+//        va_list ap;
+//        va_start(ap, path);
+//        std::shared_ptr<std::vector<std::string>> params_ptr = getParams('.', path, ap);
+//        va_end(ap);
+//
+//        if (params_ptr->size() < 2) {
+//            std::string errMsg = "SC-Conf: path cannot be created, not enough params";
+//            std::cerr << errMsg << std::endl;
+//            throw std::runtime_error(errMsg);
+//        }
+//
+//        std::vector<ConfigNode*> nodes;
+//        std::vector<std::string>& params = *params_ptr;
+//        collect(this->configRoot.get(), &params, 0, &nodes);
+//
+//        if (!nodes.empty()) {
+//            return;
+//        }
+//
+//        ConfigNode* currentNode = this->configRoot.get();
+//        size_t ind = 0;
+//        while (ind < params.size()) {
+//            auto children = currentNode->findChildren(params[ind]);
+//            if (children.empty()) {
+//                if (ind == params.size() - 1) {
+//                    currentNode = currentNode->create(params[ind], stringify(value));
+//                } else {
+//                    currentNode = currentNode->create(params[ind]);
+//                }
+//            } else {
+//                // We chose the first,  should be the only one !
+//                currentNode = children[0].get();
+//            }
+//            ++ind;
+//        }
+//    }
 };
 
 template <>
